3128c7f7d9a6bb7b5748e08af49917edee96bbb8
Morpheye <morpheyespambot@gmail.com>
2023-06-05 10:45:24 +1000

    update Arguments.java

--- src/mcpk/Arguments.java
+++ src/mcpk/Arguments.java
@@ -18,11 +18,11 @@ public class Arguments extends HashMap<String,Object>{
 		
 		this.put("blocking", 0.0); //sword block
 		this.put("slip", 0.6D); //0.8D for ice, 0.98D for ice
+		this.put("swiftness", 0.0);
+		this.put("slowness", 0.0);
 		
 		//TODO
 		this.put("soulsand", 0.0);
-		this.put("swiftness", 0.0);
-		this.put("slowness", 0.0);
 		
 	}
 
